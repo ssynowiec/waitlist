5be3f6948a27f11589c03bd1127378d5bbbbae03
Stanisław Synowiec <stasyn1410@gmail.com>
2024-06-19 10:43:25 +0200

    add: footer & privacy policy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+const PrivacyPolicyPage = () => {
+	return (
+		<main className="mx-auto flex min-h-full max-w-3xl flex-1 flex-col gap-4 py-14">
+			<h1 className="text-center text-4xl font-bold">Privacy Policy</h1>
+			<p>Last updated: June 19, 2024</p>
+		</main>
+	);
+};
+
+export default PrivacyPolicyPage;
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
 	return (
-		<footer>
-			<p>&copy; {currentYear}</p>
+		<footer className="mx-auto flex max-w-3xl justify-between">
+			<p>&copy; {currentYear} Stanisław Synowiec. All rights reserved.</p>
+			<Link href="/privacy-policy" className="font-semibold underline">
+				Privacy policy
+			</Link>
 		</footer>
 	);
 };
